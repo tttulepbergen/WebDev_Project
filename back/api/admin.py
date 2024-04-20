b107ca0510f91d9ad2eaa5fe89d6f64227f3c3d9
tttulepbergen <tulepbergen.anel@mail.ru>
2024-04-20 12:59:19 +0600

    commit

--- back/api/admin.py
+++ back/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Ingredient)
+admin.site.register(Recipe)
+admin.site.register(RecipeIngredient)
+admin.site.register(Review)
+admin.site.register(Favorite)
+admin.site.register(Category)
--- back/api/models.py
+++ back/api/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)    
+    category = models.CharField(max_length=100)  # Например: мясо, овощи, фрукты и т. д.
+    image = models.ImageField(upload_to='ingredients', null=True, blank=True)
+    def __str__(self):        
+        return self.name
+    
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)    
+    description = models.TextField()
+    ingredients = models.ManyToManyField(Ingredient, through='RecipeIngredient')    
+    instructions = models.TextField()
+    categories = models.ManyToManyField('Category')    
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)   
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+class RecipeIngredient(models.Model):   
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)    
+    quantity = models.CharField(max_length=50)
+
+class Review(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()    
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Category(models.Model):    
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Favorite(models.Model):    
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    class Meta:        
+        unique_together = ('recipe', 'user')
